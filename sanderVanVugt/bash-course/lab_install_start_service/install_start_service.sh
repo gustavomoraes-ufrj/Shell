2fc21e376be7ffa7133ed1bbc9e91e1a9cc7209f
Gustavo Moraes <gustavomoraes.ufrj@gmail.com>
2024-03-19 05:25:51 +0000

    install_start_service

---
+++ sanderVanVugt/bash-course/lab_install_start_service/install_start_service.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#todo: build a solution to detect main binary from package in case package name doesnt match the service name 
+if [ -z $1 ]
+then
+	echo provide an argument
+	exit 3
+fi
+
+if [ $MEMFREE -lt 256 ]
+then
+	echo insufficient memory
+	exit 4
+fi
+
+sudo echo apt install -y $@
+ 
+for s in "$@"
+do
+	sudo echo systemctl enable --now $s
+done
